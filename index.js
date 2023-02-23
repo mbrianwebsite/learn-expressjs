feea22860336b0507c0f28069db11b33098b1910
MBrian Website <muhammad.brian.27@gmail.com>
2023-02-23 18:44:46 +0700

    SET UP REST API

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require("express");
-const req = require("express/lib/request");
+
+const { v4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
 
-const todos = [];
+let todos = [];
 
 app.get("/todos", (req, res) => {
+  return res.json(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const todo = todos.find((todo) => todo.id === id);
+  return res.json(todo);
+});
+
+app.post("/todos", (req, res) => {
+  const { title } = req.body;
+  const todo = {
+    id: v4(),
+    title,
+    completed: false,
+  };
+  todos.push(todo);
+  return res.json(todo);
+});
+
+app.put("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const index = todos.findIndex((todo) => todo.id === id);
+  todos[index].completed = !todos[index].completed;
+  return res.json(todos[index]);
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  todos = todos.filter((todo) => {
+    return todo.id !== id;
+  });
   return res.json(todos);
 });
 
